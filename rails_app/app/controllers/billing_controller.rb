b0ff570c4292e3e7df938fc8b530c710b14a1a6b
Guillaume Caillaud <guillaume.caillaud@supelec.fr>
2018-01-06 20:37:08 +0100

    Part 1 Level 2

--- rails_app/app/controllers/billing_controller.rb
+++ rails_app/app/controllers/billing_controller.rb
@@ -4,23 +4,37 @@ class BillingController < ApplicationController
 		
 		providers = params["providers"]
 		users = params["users"]
+		contracts = params["contracts"]
 		
 		bills = Array.new
-		id = 1
+		user_consumption = {}
+		provider_ppkwh = {}
 		for user in users
-			consumption = user["yearly_consumption"].to_f
+			user_consumption[user["id"].to_s] = user["yearly_consumption"]
+		end
+		for provider in providers
+			provider_ppkwh[provider["id"].to_s] = provider["price_per_kwh"]
+		end
+		
+		
+		id = 1
+		for contract in contracts
+			consumption = user_consumption[contract["user_id"].to_s].to_f
+			ppkwh = provider_ppkwh[contract["provider_id"].to_s].to_f
 			
-			for provider in providers
-				if provider["id"] == user["provider_id"]
-					ppkwh = provider["price_per_kwh"].to_f
-				end
+			if contract["contract_length"] <= 1
+				discount = 0.9
+			elsif contract["contract_length"] <= 3
+				discount = 0.8
+			else
+				discount = 0.75
 			end
 			
-			price = consumption * ppkwh
+			price = consumption * ppkwh * discount * contract["contract_length"].to_f
 			bills.push({
 				"id": id,
-				"price": price.to_i,
-				"user_id": user["id"]
+				"price": price,
+				"user_id": contract["user_id"]
 			})
 			id+=1
 		end
